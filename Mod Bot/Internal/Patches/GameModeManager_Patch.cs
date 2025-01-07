759845d0fe2454533cd441ad6307c199448c1ee8
A TVCat <ai.grishanoff@yandex.ru>
2025-01-07 23:31:10 +0300

    start in vr main menu

---
+++ Mod Bot/Internal/Patches/GameModeManager_Patch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace InternalModBot
+{
+    [HarmonyPatch(typeof(GameModeManager))]
+    static class GameModeManager_Patch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(GameModeManager.ShouldStartVRGameFromTitleScreen))]
+        static void ShouldStartVRGameFromTitleScreen_Postfix(ref bool __result)
+        {
+            if (!__result && !WorkshopLevelManager.Instance.HasAfterSceneLoadPlaytest())
+            {
+                __result = true;
+            }
+        }
+    }
+}
